eb6ac295579a723f3c41a8b9990883eec51db6fc
placiano <nextbluerom@outlook.es>
2015-01-30 16:53:38 +0100

    Revert "Linux 3.10.19"
    This reverts commit da21c211ecd91fd8159cd7e0101a16d15073eb37.

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -84,8 +84,6 @@ kallsyms()
 
 	kallsymopt="${kallsymopt} --page-offset=$CONFIG_PAGE_OFFSET"
 
-	kallsymopt="${kallsymopt} --page-offset=$CONFIG_PAGE_OFFSET"
-
 	local aflags="${KBUILD_AFLAGS} ${KBUILD_AFLAGS_KERNEL}               \
 		      ${NOSTDINC_FLAGS} ${LINUXINCLUDE} ${KBUILD_CPPFLAGS}"
 
